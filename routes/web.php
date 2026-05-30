438db1ffff0b3f9a0f02921c5eefc6996fe244e8
Apple <apple@appurunoMacBook-Air.local>
2026-05-30 12:34:24 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -124,7 +124,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/settings/password', [PasswordController::class, 'edit'])
         ->name('password.edit');
 
-    Route::put('/settings/password', [PasswordController::class, 'update'])
+    Route::post('/settings/password', [PasswordController::class, 'update'])
         ->middleware('throttle:5,1')
         ->name('password.update');
 
