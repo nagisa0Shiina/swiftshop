c827528facf1fd5e52843c10ccf2e0f428572e68
Apple <apple@appurunoMacBook-Air.local>
2026-06-02 14:37:31 +0900

    update

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -229,7 +229,7 @@
                     </a>
 
                     <a href="{{ route('orders.index') }}"
-                       class="site-mobile-link flex items-center justify-between rounded-2xl px-5 py-4 bg-gray-50 text-[#111827]   font-bold">
+                       class="site-mobile-link flex items-center justify-between rounded-2xl px-5 py-4 bg-[#070d16] text-white  font-bold">
                         <span>注文履歴</span>
                         <i data-lucide="receipt-text" class="w-5 h-5"></i>
                     </a>
